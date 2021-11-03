402cf5207b39a4131a247fa855e36ec1baf168f5
Patrick Johan Cuentas Mariano <patrick_cuentas@hotmail.com>
2021-11-03 10:26:39 -0500

    First Commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
+
+const GifExpertApp = ({ props }) => {
+  const [categories, setCategories] = useState(["One Punch Man"]);
+
+  return (
+    <>
+      <h2>GifExpertApp</h2>
+
+      <AddCategory setCategories={setCategories} />
+
+      <hr />
+
+      {categories.map((category) => (
+        <GifGrid key={category} category={category} />
+      ))}
+      
+    </>
+  );
+};
+
+export default GifExpertApp;
---
+++ src/helpers/getGifs.js
@@ -0,0 +1,17 @@
+export const getGifs = async (category) => {
+  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(
+    category
+  )}&limit=10&api_key=6wFW7z1QzgfOfkWXUFsFANV5PYGcV83r`;
+  const resp = await fetch(url);
+  const { data } = await resp.json();
+
+  const gifs = data.map((img) => {
+    return {
+      id: img.id,
+      title: img.title,
+      url: img.images?.original.url,
+    };
+  });
+
+  return gifs;
+};
